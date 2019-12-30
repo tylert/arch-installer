b69f9a08b548dd6b3ee36107c4371238e248bf88
Tyler Tidman <tyler.tidman@draak.ca>
2019-12-30 18:37:44 -0500

    Add 64-bit support for Odroid C2?

--- flash_c2.sh
+++ flash_c2.sh
@@ -24,7 +24,7 @@ EOF
 if [ "${root_filesystem_type}" = 'btrfs' ]; then
     mkfs.btrfs --force --label os "${first_partition}"
 else
-    mkfs.ext4 -F -L os "${first_partition}"
+    mkfs.ext4 -O ^metadata_csum,^64bit -F -L os "${first_partition}"
 fi
 
 # Mount the drive and create the necessary locations
