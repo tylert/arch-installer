8e01fc90ae17624b43308b0c9f9735db2bb1f8ba
Tyler Tidman <tyler.tidman@draak.ca>
2021-01-13 20:53:59 -0500

    Add a non-UEFI install script too

--- install_x86.sh
+++ install_x86.sh
@@ -3,7 +3,6 @@
 # Kick off the full install procedure and then configure the chroot
 
 # Install assumptions:
-# - UEFI
 # - SSD >= (2 * RAM) + 1 GB + n GB
 # - swap is RAM + "a bit" (hibernate)
 # - unencrypted root
@@ -29,36 +28,31 @@ fi
 dd if=/dev/zero of="${DRIVE}" bs=1M count=8
 echo 'label: gpt' | sfdisk --no-reread --force "${DRIVE}"
 sfdisk --no-reread --force "${DRIVE}" << EOF
-,256M,U
 ,20G,S
 ,
 EOF
 partx "${DRIVE}"
 # might have to "dmsetup remove /dev/mapper/foo"
 
-mkfs.vfat -F 32 "${DRIVE}1"
-mkswap "${DRIVE}2"
-mkfs.btrfs --force --label os "${DRIVE}3"
+mkswap "${DRIVE}1"
+mkfs.btrfs --force --label os "${DRIVE}2"
 
 # ---==[ Create subvolumes and mount subordinate partitions ]==----------------
 if [ -z "${MOUNT}" ]; then
     MOUNT='/mnt'
 fi
 
-mount "${DRIVE}3" "${MOUNT}" --options subvolid=5
+mount "${DRIVE}2" "${MOUNT}" --options subvolid=5
 btrfs subvolume create "${MOUNT}/@"
 umount "${MOUNT}"
-mount "${DRIVE}3" "${MOUNT}" --options subvol=@
+mount "${DRIVE}2" "${MOUNT}" --options subvol=@
 
-mkdir --parents "${MOUNT}/boot/EFI"
-mount "${DRIVE}1" "${MOUNT}/boot/EFI"
-swapon "${DRIVE}2"
+swapon "${DRIVE}1"
 
 # ---==[ Install the OS and build the fstab file ]==---------------------------
 timedatectl set-ntp true
 pacstrap "${MOUNT}" base linux linux-firmware btrfs-progs
 # basestrap "${MOUNT}" base
-# manjaro https://forum.manjaro.org/t/howto-install-manjaro-using-cli-only/108203
 
 cat << EOF > "${MOUNT}/etc/fstab"
 # Static information about the filesystems.
@@ -69,9 +63,9 @@ EOF
 genfstab -p -t UUID "${MOUNT}" >> "${MOUNT}/etc/fstab"
 
 # ---==[ Configure the new system ]==------------------------------------------
-cp configure_x86_uefi.sh "${MOUNT}/root/"
-arch-chroot "${MOUNT}" /root/configure_x86_uefi.sh
-rm "${MOUNT}/root/configure_x86_uefi.sh"
+cp configure_x86.sh "${MOUNT}/root/"
+arch-chroot "${MOUNT}" /root/configure_x86.sh
+rm "${MOUNT}/root/configure_x86.sh"
 
 # ---==[ Unmount everything ]==------------------------------------------------
 # swapoff "${DRIVE}2"
